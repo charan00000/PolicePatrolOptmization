599fd139a38af7cb41f778754efe8eaa0f2db972
charan00000 <108594418+charan00000@users.noreply.github.com>
2023-11-21 04:22:30 -0500

    Add files via upload

--- find_euler_path.py
+++ find_euler_path.py
@@ -1,37 +1,86 @@
+import ast
 import networkx as nx
 import matplotlib.pyplot as plt
+import heapq
+from math import radians, sin, cos, sqrt, atan2
 
-def builtin_method(graphml_input = 'new_graph.graphml', dest = 'euler_path_output.graphml'):
+def modify_graph(graphml_input = 'new_graph.graphml', dest = 'euler_path_output.graphml', method = "base"):
     G = nx.read_graphml(graphml_input)
-    euler_G = custom_eulerize(G)
+    if method == "min_weights":
+        euler_G = eulerize_minimize_weights(G)
+    else:
+        euler_G = eulerize_base(G)
     circuit = list(nx.eulerian_circuit(euler_G))
+    #print(circuit)
     new_G = nx.MultiDiGraph(nx.create_empty_copy(euler_G))
 
-    for start, target in circuit:
-        edge_data = euler_G.get_edge_data(start, target)
-        if edge_data is None:
+    for source, target in circuit:
+        edge_data = euler_G.get_edge_data(source, target)
+        if (len(edge_data[0]) == 0 or len(edge_data) == 0 or 'name' not in edge_data[0]):
             road_name = "unnamed"
         else:
             road_name = edge_data[0]['name']
-        new_G.add_edge(start, target, name = road_name)
+            new_G.add_edge(source, target, name = road_name, length = edge_data[0]['length'])
 
     nx.write_graphml(new_G, dest)
 
-def custom_eulerize(G):
+    total_distance = 0
+    for (source, target) in circuit:
+        if new_G.get_edge_data(source, target) is None:
+            source = ast.literal_eval(source)
+            target = ast.literal_eval(target)
+            total_distance += calculate_distance(source[0], source[1], target[0], target[1])
+        else: 
+            total_distance += new_G.get_edge_data(source, target)[0]['length']
+    return total_distance
+
+def eulerize_base(G):
+    return nx.eulerize(G)
+
+def eulerize_minimize_weights(old_G):
     # Create a copy of the graph to avoid modifying the original graph
-    G = G.copy()
+    G = old_G.copy()
+    print("started eulerize_minimize_weights")
     # Find all nodes with odd degree
     odd_degree_nodes = [node for node, degree in G.degree() if degree % 2 == 1]
+    #print("odd degree nodes: ", odd_degree_nodes)
+
+    # Calculate shortest paths between all pairs of odd-degree nodes
+    shortest_paths = dict(nx.all_pairs_shortest_path(G))
+
+    # Create a priority queue of pairs of odd-degree nodes, with distances as priorities
+    pair_queue = [(len(shortest_paths[node1][node2]), node1, node2) for i, node1 in enumerate(odd_degree_nodes) for node2 in odd_degree_nodes[i+1:]]
+    heapq.heapify(pair_queue)
+
     # While there are nodes with odd degree
-    while odd_degree_nodes:
-        # Pick the first node with odd degree
-        node1 = odd_degree_nodes.pop(0)
-        # Try to find another node with odd degree that is connected to node1
-        for node2 in odd_degree_nodes:
-            if G.number_of_edges(node1, node2) > 0:
-                # Add an edge between node1 and node2
-                G.add_edge(node1, node2)
-                # Remove node2 from the list of nodes with odd degree
-                odd_degree_nodes.remove(node2)
-                break
-    return G
+    while pair_queue:
+    
+        # Pop the pair with the shortest distance
+        _, node1, node2 = heapq.heappop(pair_queue)
+
+        if node1 in odd_degree_nodes and node2 in odd_degree_nodes:
+
+            # Add an edge between node1 and node2
+            G.add_edge(node1, node2)
+
+            # Remove node1 and node2 from the list of nodes with odd degree
+            odd_degree_nodes.remove(node1)
+            odd_degree_nodes.remove(node2)
+    odd_degree_nodes = [node for node, degree in G.degree() if degree % 2 == 1]
+    print("odd degree nodes: ", odd_degree_nodes)
+    return nx.eulerize(G)
+
+def calculate_distance(lon1, lat1, lon2, lat2):
+    # approximate radius of earth in miles
+    R = 3958.8
+
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1 - a))
+
+    distance = R * c
+    return distance
--- main_driver.py
+++ main_driver.py
@@ -1,6 +1,42 @@
 import conversions
 import find_euler_path
+import geopandas as gpd
+import matplotlib.pyplot as plt
 
-conversions.convert_to_graph_road_edges('forsyth_major_bottom_left_roads.geojson', dest = 'forsyth_major_bottom_left_roads.graphml')
-find_euler_path.builtin_method(graphml_input = 'forsyth_major_bottom_left_roads.graphml', dest = 'euler_path_output.graphml')
+conversions.convert_to_graph_road_edges('forsyth_major_bottom_left_roads.geojson', dest = 'forsyth_major_bottom_left_roads.graphml', has_properties = True)
+total_distance = find_euler_path.modify_graph(graphml_input = 'forsyth_major_bottom_left_roads.graphml', dest = 'euler_path_output.graphml', method = "min_weiglhts")
 conversions.convert_to_geojson('euler_path_output.graphml')
+print(total_distance)
+
+def plot(geojson_file, time_delay = 0):
+    # Load the GeoJSON file
+    gdf = gpd.read_file(geojson_file)
+
+    # Create a new plot
+    fig, ax = plt.subplots()
+
+    # Iterate over the GeoDataFrame
+    for i, row in gdf.iterrows():
+        # Get the line's start and end points
+        if row['geometry'].type == 'MultiLineString' or row['geometry'].geom_type == 'MultiPolygon':
+            start = row['geometry'][0].coords[0][:2]
+            end = row['geometry'][-1].coords[-1][:2]
+        else:
+            start = row['geometry'].coords[0][:2]
+            end = row['geometry'].coords[-1][:2]
+
+        # Plot the line
+        ax.plot(*row['geometry'].xy, color='blue')
+
+        # Add an arrow at the end of the line to represent the direction
+        ax.annotate('', xy=end, xytext=start,
+                    arrowprops=dict(facecolor='red', edgecolor='red'))
+
+        # Add a delay
+        if time_delay > 0:
+            plt.pause(time_delay)
+
+    # Show the plot
+    plt.show()
+
+#plot('output_geojson.geojson')
