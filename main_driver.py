2a798eaffe5ffc725b47964ae7f69dd98016ae15
charan00000 <gkoyaguri3@gatech.edu>
2023-11-22 02:02:39 -0500

    rem

--- main_driver.py
+++ main_driver.py
@@ -4,9 +4,10 @@ import find_euler_path
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-#scroll down
 
-def plot(geojson_file, time_delay = 0, arrow_spacing = 15):
+# scroll down
+
+def plot(geojson_file, time_delay=0, arrow_spacing=15):
     """
     Plots the lines from a GeoJSON file and adds arrows to represent the direction of the lines.
     Supports delay while graphing to better visualize the direction of path
@@ -42,15 +43,16 @@ def plot(geojson_file, time_delay = 0, arrow_spacing = 15):
         if i % arrow_spacing == 0:
             ax.annotate('', xy=end, xytext=start, arrowprops=dict(facecolor='red', edgecolor='red'))
 
-        #fig.canvas.draw()
+        # fig.canvas.draw()
 
         # Add a delay
         if time_delay > 0:
             plt.pause(time_delay)
-    
+
     # Show the plot
-    #ctx.add_basemap(ax)
+    # ctx.add_basemap(ax)
     plt.show()
+
 
 """
 The lines below are the important ones. Here, you can run all the code from the rest
@@ -79,17 +81,17 @@ this output is later printed to the console.
 """
 
 conversions.convert_to_graph_road_edges('forsyth_major_bottom_left_roads.geojson',
-                                        dest = 'forsyth_major_bottom_left_roads.graphml',
-                                        formatted_road_name = 'FullStName',    # road name label
-                                        formatted_road_type = 'RoadPosTyp',    # road type label
-                                        has_properties = True,
-                                        length_unit = 'miles',
-                                        weighted_by_road_type = True)  # toggle for multiplying busy roads
+                                        dest='forsyth_major_bottom_left_roads.graphml',
+                                        formatted_road_name='FullStName',  # road name label
+                                        formatted_road_type='RoadPosTyp',  # road type label
+                                        has_properties=True,
+                                        length_unit='miles',
+                                        weighted_by_road_type=True)  # toggle for multiplying busy roads
 
-attributes = find_euler_path.modify_graph(graphml_input = 'forsyth_major_bottom_left_roads.graphml',
-                                              dest = 'euler_path_output.graphml',
-                                              method = "dijkkstra",
-                                              length_unit = "miles")
+attributes = find_euler_path.modify_graph(graphml_input='forsyth_major_bottom_left_roads.graphml',
+                                          dest='euler_path_output.graphml',
+                                          method="dijkkstra",
+                                          length_unit="miles")
 
 conversions.convert_to_geojson('euler_path_output.graphml')
 print(attributes)
